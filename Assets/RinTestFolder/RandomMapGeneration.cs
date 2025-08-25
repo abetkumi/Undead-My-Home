5ca4426e8194cd9290d68944dd00a7a820df0d59
rin3432 <ca01234007@st.kawahara.ac.jp>
2025-08-25 13:52:49 +0900

    Stage

---
+++ Assets/RinTestFolder/RandomMapGeneration.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMapGeneration : MonoBehaviour
+{
+    enum MapSelect
+    {
+        None,
+        Map1,
+        Map2,
+        Map3,
+        Num,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
