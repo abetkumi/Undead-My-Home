d496ca51b4a57fabddd3a2d6b932851e7d0c1457
abetkumi <ca01234001@st.kawahara.ac.jp>
2025-08-25 14:18:37 +0900

    プレイヤー移動追加

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,122 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private enum PlayerState
+    {
+        Idle,
+        Move,
+        Dead,
+    }
+
+    //パラメータ
+    [SerializeField] float m_moveSpeed = 500.0f;
+    float m_walkSpeed = 500.0f;
+    float m_runSpeed = 1000.0f;
+    float t = 0.5f;
+    PlayerState m_playerState = PlayerState.Idle;
+
+    Vector3 PlayerMove = Vector3.zero;
+    Vector3 stickL = Vector3.zero;
+
+    //キャッシュ
+    Rigidbody m_rigidBody;
+    GameObject m_gameCameraObj;
+    GameObject m_playerObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //必要な情報を取得
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_gameCameraObj = Camera.main.gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlayerStatus()
     {
-        
+        switch (m_playerState)
+        {
+            case PlayerState.Idle:
+                Idle();
+                break;
+            case PlayerState.Move:
+                Move();
+                break;
+            case PlayerState.Dead:
+                break;
+        }
+    }
+
+    void Idle()
+    {
+        stickL.z = Input.GetAxis("Vertical");
+        stickL.x = Input.GetAxis("Horizontal");
+        if (stickL.magnitude > 0.1f)
+        {
+            m_playerState = PlayerState.Move;
+        }
+    }
+
+    void Move()
+    {
+        //カメラを考慮した移動
+        PlayerMove = Vector3.zero;
+        stickL = Vector3.zero;
+        stickL.z = Input.GetAxis("Vertical");
+        stickL.x = Input.GetAxis("Horizontal");
+        if (stickL.magnitude <= 0.1f)
+        {
+            m_playerState = PlayerState.Idle;
+        }
+
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+        forward.y = 0.0f;
+        right.y = 0.0f;
+
+        right *= stickL.x;
+        forward *= stickL.z;
+
+        //移動速度に上記で計算したベクトルを加算する
+        PlayerMove += right + forward;
+
+        //Runキーが押されている場合
+        if (Input.GetButton("Run"))
+        {
+            m_moveSpeed = m_runSpeed;
+        }
+        else
+        {
+            m_moveSpeed = m_walkSpeed;
+        }
+
+        //プレイヤーの速度を設定することで移動させる
+        PlayerMove = (PlayerMove * m_moveSpeed * Time.deltaTime);
+        PlayerMove.y = m_rigidBody.velocity.y;
+        m_rigidBody.velocity = PlayerMove;
+
+
+        if (stickL != Vector3.zero)
+        {
+            t += Time.deltaTime;
+            if (t > 0.5f)
+            {
+                t = 0.0f;
+            }
+        }
+        else if (stickL == Vector3.zero)
+        {
+            t = 0.0f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //プレイ中でないなら中断
+        if (GameManager.GetGameState() != GameManager.GameState.enGameState_Play)
+        {
+            return;
+        }
+
+        PlayerStatus();
     }
 }
