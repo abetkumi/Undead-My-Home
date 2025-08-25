90266b6a6cfde224bd9730a5f06c73eb99c57ceb
abetkumi <ca01234001@st.kawahara.ac.jp>
2025-08-25 14:18:37 +0900

    プレイヤー移動追加

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //ゲームの状態
+    public enum GameState
+    {
+        enGameState_Play,
+        enGameState_Clear,
+    }
+    static GameState m_gameState = GameState.enGameState_Play;
+
+    public void SetGameState(GameState gameState)
+    {
+        m_gameState = gameState;
+    }
+    //どこからでも呼び出せる関数
+    public static GameState GetGameState()
+    {
+        return m_gameState;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ステートの更新（初期化）
+        m_gameState = GameState.enGameState_Play;
     }
 
     // Update is called once per frame
