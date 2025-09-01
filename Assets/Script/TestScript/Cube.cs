5629d613bbe72529ecdce2ad0f19f2645e850a66
rin3432 <ca01234007@st.kawahara.ac.jp>
2025-09-01 14:44:58 +0900

    cubeScript

--- Assets/Script/TestScript/Cube.cs
+++ Assets/Script/TestScript/Cube.cs
@@ -13,41 +13,82 @@ public class Cube : MonoBehaviour
     Point[] point = new Point[100];
 
     //キューブの個数。
-    int count = 0;
+    int cubeNum = 0;
+    //設置したポイントの個数。
+    int pointNum = 0;
+
+    Transform parent;
 
     // Start is called before the first frame update
     void Start()
     {
-        SetCount();
+        SetCube();
+        SetPoint();
 
-        for(int i = 1; i <= count; i++){
-            cubes[i] = GameObject.Instantiate(cubePrefab);
-            point[i] = PointList.GetComponent<Point>();
-        }
+        PointSelect(cubeNum);
     }
 
     //キューブの個数設定。
-    void SetCount()
+    void SetCube()
     {
-        count = 3;
+        cubeNum = 2;
+
+        for (int i = 0; i < cubeNum; i++){
+            cubes[i] = GameObject.Instantiate(cubePrefab);
+        }
     }
 
-    //ポイントを選択。(引数には設置するcubeの数を代入)
-    void PointSelect(int count)
+    //ポイントの個数設定。
+    void SetPoint()
     {
+        bool found = false;
+
         //ポイントリストが存在するか検索。
-        Transform parent = GameObject.Find("PointList")?.transform;
+        parent = GameObject.Find("PointList")?.transform;
         if (parent == null)
         {
             Debug.LogWarning("PointList が見つかりませんでした");
             return;
         }
 
-        //リスト内の未使用のポイントを検索。
-        for (int i = 1; i <= count; i++){
-            cubes[i] = GameObject.Instantiate(cubePrefab);
+        while (!found){
+            string name = "Point" + (pointNum + 1).ToString("D3");
+            Transform obj = parent.Find(name);
+            if (obj != null)
+            {
+                point[pointNum] = new Point();
+                pointNum++;
+            }
+            else found = true;
         }
     }
+
+    //ポイントを選択。(引数には設置するcubeの数を代入)
+    void PointSelect(int count)
+    {
+        //リスト内の未使用のポイントを検索。
+        for (int i = 0; i < count; i++){
+            bool found = false;
+
+            while (!found){
+                int pointNo = Random.Range(1, pointNum + 1);
+
+                if (!point[pointNo - 1].GetUes()){
+                    string name = "Point" + pointNo.ToString("D3");
+                    Transform obj = parent.Find(name);
+
+                    if (obj != null)
+                    {
+                            cubes[i].transform.position = obj.transform.position;
+                            point[pointNo - 1].SetUesTrue();
+                            found = true;
+                    }
+                }
+                else{}
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
