0c3e13d08d29a03f6108158fac8e515f4f95d7ef
abetkumi <ca01234001@st.kawahara.ac.jp>
2026-01-16 11:33:48 +0900

    GaneOverのシーン追加

--- Assets/Script/SceneChange/TitleBack.cs
+++ Assets/Script/SceneChange/TitleBack.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,29 @@ using UnityEngine;
 public class TitleBack : MonoBehaviour
 {
     [SerializeField] GameObject m_fadeCanvas;
-    // Update is called once per frame
-    void Update()
+    [SerializeField] GameObject m_gameOverLightObject;
+    [SerializeField] AudioClip m_lightSE;
+
+    async void TitleBackSence()
     {
+        await UniTask.Delay(2000);
+        if(m_gameOverLightObject != null)
+        {
+            Destroy(m_gameOverLightObject);
+            GameManager.PlaySE(m_lightSE);
+        }
+
         if (Input.anyKeyDown)
         {
             GameObject fadeObject = Instantiate(m_fadeCanvas);
             fadeObject.GetComponent<FadeScene>().FadeStart("TitleScene", Color.black, true);
             Destroy(gameObject);
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TitleBackSence();
     }
 }
